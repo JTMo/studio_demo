5a0e9b71cb973560c7babf4c33f46ed9a6802f12
moleiyi <mail.jiehun.com.cn>
2014-12-18 17:27:53 +0800

    实现侧滑不显示actionbar

--- app/src/main/java/com/example/myapplication/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/ui/activity/MainActivity.java
@@ -6,8 +6,11 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.FrameLayout;
@@ -16,6 +19,7 @@ import com.example.myapplication.R;
 import com.example.myapplication.manager.BaseApplication;
 import com.example.myapplication.ui.fragment.MainFragment;
 import com.example.myapplication.ui.fragment.MenuFragment;
+import com.example.myapplication.utils.UIUtils;
 
 
 public class MainActivity extends BaseActivity {
@@ -23,9 +27,11 @@ public class MainActivity extends BaseActivity {
     private SlidingPaneLayout mSlidingPaneLayout;
     private MenuFragment mMenu;
     private android.support.v4.app.FragmentTransaction transaction;
-    private DisplayMetrics displayMetrics=new DisplayMetrics();
+    private DisplayMetrics displayMetrics = new DisplayMetrics();
     private MainFragment mMain;
     private int maxMargin;
+    private ActionBar mActionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +44,23 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initActionBar() {
-        ActionBar mActionBar=getSupportActionBar();
+        mActionBar = getSupportActionBar();
         mActionBar.setTitle("婚博会");
-        
+        mActionBar.setBackgroundDrawable(UIUtils.getDrawable(R.drawable.actionbar));
+
     }
 
     private void initView() {
         mSlidingPaneLayout = (SlidingPaneLayout) findViewById(R.id.slidingpanellayout);
         mMenu = new MenuFragment();
         mMain = new MainFragment();
-        transaction=getSupportFragmentManager().beginTransaction();
+        transaction = getSupportFragmentManager().beginTransaction();
 
-        transaction.replace(R.id.slidingpane_menu,mMenu);
-        transaction.replace(R.id.slidingpane_content,mMain);
+        transaction.replace(R.id.slidingpane_menu, mMenu);
+        transaction.replace(R.id.slidingpane_content, mMain);
         transaction.commit();
 
-        maxMargin = displayMetrics.heightPixels/10;
+        maxMargin = displayMetrics.heightPixels / 10;
         mSlidingPaneLayout.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
             @Override
             public void onPanelSlide(View view, float slideOffset) {
@@ -73,19 +80,21 @@ public class MainActivity extends BaseActivity {
                 mMenu.getCurrentView().setPivotY(
                         displayMetrics.heightPixels / 2);
                 mMenu.getCurrentView().setAlpha(slideOffset);
+
             }
 
             @Override
             public void onPanelOpened(View view) {
-
+                mActionBar.hide();
             }
 
             @Override
             public void onPanelClosed(View view) {
-
+                mActionBar.show();
             }
         });
     }
+
 
 
     @Override
@@ -94,5 +103,15 @@ public class MainActivity extends BaseActivity {
         return true;
     }
 
-
+    /**
+     * 返回键监听
+     *
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        return super.onKeyUp(keyCode, event);
+    }
 }
--- app/src/main/java/com/example/myapplication/ui/fragment/MainFragment.java
+++ app/src/main/java/com/example/myapplication/ui/fragment/MainFragment.java
@@ -3,10 +3,12 @@ package com.example.myapplication.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ScrollView;
 
 import com.example.myapplication.R;
 
@@ -17,12 +19,25 @@ public class MainFragment extends Fragment {
 
     private View mCurrentView;
 
+    private ActionBar actionBar;
+    public MainFragment(){
+
+    }
+
+    public MainFragment(ActionBar actionBar){
+      this.actionBar=actionBar;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mCurrentView= inflater.inflate(R.layout.fragment_content,null);
+        ScrollView scrollView= (ScrollView) mCurrentView.findViewById(R.id.scroll);
 
         return mCurrentView;
     }
+
+
+
 
     public FrameLayout.LayoutParams getCurrentViewParams(){
         return (FrameLayout.LayoutParams) mCurrentView.getLayoutParams();
